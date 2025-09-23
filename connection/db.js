f907d8f12162bd68f90f92bdd3b58a943a7426c9
Sachin Jha <sachin03.nic@gmail.com>
2025-09-23 14:24:01 +0530

    mongodb connection

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/revital").then(() => {
+// mongoose.connect("mongodb://127.0.0.1:27017/revital").then(() => {
+mongoose.connect("mongodb+srv://sachin03:sachin03@cluster0.n3gnzav.mongodb.net/revital?retryWrites=true&w=majority&appName=Cluster0").then(() => {
     console.log("Connected To Database Successfully");
 }).catch((err) => {
     console.log("Failed to connect with Database " , (err) );
